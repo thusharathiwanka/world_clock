77bdb1e5b3c41cad761a982ab3eeaf685edad8bd
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-03-05 15:46:48 +0530

    CREATE: routing setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:world_clock/screens/change_location.dart';
 import 'package:world_clock/screens/home.dart';
+import 'package:world_clock/screens/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => const Loading(),
+      '/home': (context) => const Home(),
+      '/location': (context) => const ChangeLocation(),
+    },
   ));
 }
