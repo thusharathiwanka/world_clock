2b655c8bb7ed167340cb2d60e5f6ece300e822f1
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-03-08 15:48:41 +0530

    CREATE: error handling added

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -10,11 +10,15 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
   void setupWorldTime() async {
     WorldTime worldTime = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await worldTime.getTime();
-    print(worldTime.time);
+    setState(() {
+      time = worldTime.time;
+    });
   }
 
   @override
@@ -25,6 +29,6 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Loading'));
+    return Scaffold(body: SafeArea(child: Text(time)));
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -3,23 +3,27 @@ import 'dart:convert';
 
 class WorldTime {
   String location;
-  String? time;
+  late String time;
   String flag;
   String url;
 
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    Response response =
-        await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
+    try {
+      Response response =
+          await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+      time = 'Could not get the time';
+    }
   }
 }
